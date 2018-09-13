33483ff8eee04bf7784e361bdb0a0f9b147d2cb7
XxIngY <1391065515@qq.com>
2018-09-13 15:40:53 +0800

    添加测试代码

--- src/CountUtilsTest.java
+++ src/CountUtilsTest.java
@@ -6,20 +6,20 @@ public class CountUtilsTest {
 	
 	@Test
 	public void testCountChar() throws IOException{
-		CountUtils.countChar("1.txt");
-		CountUtils.countChar("2.txt");
-		CountUtils.countChar("3.txt");
+		CountUtils.countChar("test/1.txt");
+		CountUtils.countChar("test/2.txt");
+		CountUtils.countChar("test/3.txt");
 	}
 	@Test
 	public void testCountWord() throws IOException{
-		CountUtils.countWord("1.txt");
-		CountUtils.countWord("2.txt");
-		CountUtils.countWord("3.txt");
+		CountUtils.countWord("test/1.txt");
+		CountUtils.countWord("test/2.txt");
+		CountUtils.countWord("test/3.txt");
 	}
 	@Test
 	public void testCountLine() throws IOException{
-		CountUtils.countLine("1.txt");
-		CountUtils.countLine("2.txt");
-		CountUtils.countLine("3.txt");
+		CountUtils.countLine("test/1.txt");
+		CountUtils.countLine("test/2.txt");
+		CountUtils.countLine("test/3.txt");
 	}
 }
